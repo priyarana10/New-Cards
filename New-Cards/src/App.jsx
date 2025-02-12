cde8e671adbae3f59a8ccdd2aacc7dd54ba60703
priyarana10 <priyarana4488@gmail.com>
2025-02-12 13:31:00 +0530

    new commit

--- New-Cards/src/App.jsx
+++ New-Cards/src/App.jsx
@@ -1,13 +1,38 @@
 import { useState } from 'react'
 import './App.css'
 import Update from './assets/Update/Update'
+import Gener from './assets/popular/Gener'
 
 function App() {
-  
+ const category=[
+  {
+    id:0,
+    category:"Popular",
+    name:"knock-knock",
+    year:2015,
+  },
+  // {
+  //   id:1,
+  //   category:"Comedyr",
+  //   name:"Jungle-safari",
+  //   year:2015,
+  // },
+  // {
+  //   id:2,
+  //   category:"Action",
+  //   name:"Sniper",
+  //   year:2015,
+  // },
+ ]
+ const[selectAction, setSelectedAction]=useState("")
+
 
   return (
     <>
-      <Update/>
+      <Update lists={category}  />
+      <Gener />
+      
+
     </>
   )
 }
---
+++ New-Cards/src/assets/popular/Gener.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Gener.css"
+const Gener = () => {
+  return (
+    <>
+    <div className="main">
+        <div className="lists">
+            <img src="public/action.jpg" alt="" className="photos" />
+            <h3 className="names">knock-knock</h3>
+            <p className="years">2017</p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Gener
